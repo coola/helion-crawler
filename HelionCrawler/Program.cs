d5ff784a5191e4a2f7b81e55311d469ae7b4fbce
Michał Kuliński <michal.kulinski@accenture.com>
2017-04-24 16:09:09 +0200

    Add project files.

---
+++ HelionCrawler/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace HelionCrawler
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            using (var fs = File.OpenRead(@"C:\test.csv"))
+            using (var reader = new StreamReader(fs))
+            {
+                var listA = new List<string>();
+                var listB = new List<string>();
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(';');
+
+                    listA.Add(values[0]);
+                    Console.Out.WriteLine(values[0]);
+                    listB.Add(values[1]);
+                }
+            }
+
+            Console.In.ReadLine();
+        }
+    }
+}
